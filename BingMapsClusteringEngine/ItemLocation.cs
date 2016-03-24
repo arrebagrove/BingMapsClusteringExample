387bd09bd4ffa63475a01727eaa82627f5616bce
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2016-03-24 11:43:01 +0100

    win 8.1 version added

---
+++ BingMapsClusteringEngine/ItemLocation.cs
@@ -0,0 +1,16 @@
+﻿using Bing.Maps;
+
+namespace BingMapsClusteringEngine
+{
+    public class ItemLocation
+    {
+        public ItemLocation(object item, Location location)
+        {
+            Item = item;
+            Location = location;
+        }
+
+        public object Item { get; set; }
+        public Location Location { get; set; }
+    }
+}
